fab5d7be62c216e431965621c4403b17da438f92
Albert <alcacer.la.1001@gmail.com>
2018-01-29 18:38:02 +0100

    expected result

--- fils-cooperatius.js
+++ fils-cooperatius.js
@@ -1,69 +1,41 @@
 function make_thread_system() {
-    var spawned = 0;
     var threads = [];
     var halt = null;
 
-    function make_thread(c, name) {
-        return { cont: c, name: name }
-    }
-
-    function push(c, name) {
-        threads.push(make_thread(c, name))
-        print(threads.map(t => t.name))
-    }
-
     function shift() {
         var nt = threads.shift()
-        print('Shift: ' + nt.name)
-        nt.cont(2)
+        nt()
     }
 
     return {
         spawn: function(thunk, name) {
             var c = current_continuation()
             if (c instanceof Continuation) {
-                push(c, name)
-                print('spawn push')
-                spawned++;
+                threads.push(c)
             } else {
-                print('spawn thunk')
                 thunk()
             }
         },
-        quit: function(name) {
-            // "== 2" en la imp de scheme
+        quit: function() {
             if (threads.length > 0) {
-                print('quit: remaining threads')
                 shift()
             } else {
-                print('quit: halt')
                 halt()
             }
         },
         relinquish: function(name) {
-            //Por alguna razón, los threads a, b, c que se guardan al principio, no se recuperan al hacer shift(). Se queda en el thread "name: c".
-            print('relinquish from: ' + name)
-            print('tlen: ' + threads.length)
             var c = new Continuation()
             if (c instanceof Continuation) {
-                print('relinquish push')
-                push(c, name)
-                var nt = threads.shift()
-                print('Shift: ' + nt.name)
-                nt.cont()
+                threads.push(c)
+                shift()
             }
         },
         start_threads: function() {
-            print('start')
             var c = current_continuation()
             if (c instanceof Continuation) {
                 halt = c
-                print('start call')
-                //halt = c tendria que estar aquí. Para inicializar halt con la continuación de star_threads y que al hacer halt() entre al else
                 shift()
-            } else {
-                print('finish')
-            }
+            } 
         }
     }
 }
@@ -72,18 +44,21 @@ function current_continuation() {
     return new Continuation();
 }
 
+var counter = 10;
 function make_thread_thunk(name, thread_system) {
     function loop() {
-        for (let i = 0; i < 5; i++) {
-            print('in thread', name, '; i =', i);
-            thread_system.relinquish(name);
+        if (counter < 0) {
+            thread_system.quit();
         }
-        thread_system.quit();
+        print('in thread', name, '; counter =', counter);
+        counter--;
+        thread_system.relinquish();
+        loop();
     };
     return loop;
 }
 var thread_sys = make_thread_system();
-thread_sys.spawn(make_thread_thunk('a', thread_sys), 'a');
-thread_sys.spawn(make_thread_thunk('b', thread_sys), 'b');
-thread_sys.spawn(make_thread_thunk('c', thread_sys), 'c');
+thread_sys.spawn(make_thread_thunk('a', thread_sys));
+thread_sys.spawn(make_thread_thunk('b', thread_sys));
+thread_sys.spawn(make_thread_thunk('c', thread_sys));
 thread_sys.start_threads();
